f7f48980cb9bbcb9647174407eaca964d305b2a6
NushN <NushN@nushn-book>
2014-12-23 00:22:38 +0400

    Creating first simple test

---
+++ JavaMailParser/test/javamailparsertest/JavaMailParserTest.java
@@ -0,0 +1,14 @@
+package javamailparsertest;
+
+import junit.framework.*;
+
+import javamailparser.*;
+
+public class JavaMailParserTest extends TestCase {
+    // … standard setup code …
+
+    // Test object creation
+    public void test_smoke() {
+        JavaMailParser parser = new JavaMailParser();
+    }
+}
