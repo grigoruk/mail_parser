bf28c28e09f4b4a0a16908c6cbf8f2e9d9fb8c4f
NushN <NushN@nushn-book>
2014-12-24 01:20:28 +0400

    Creating new Mail class, all mail functionality goes there. Plus some new tests for Mail class.

--- JavaMailParser/src/javamailparser/JavaMailParser.java
+++ JavaMailParser/src/javamailparser/JavaMailParser.java
@@ -1,7 +1,9 @@
 package javamailparser;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JavaMailParser {
-
+    Map<String, Integer> hm = new HashMap<String, Integer>();
     public static void main(String[] args) {
         // TODO code application logic here
     }
